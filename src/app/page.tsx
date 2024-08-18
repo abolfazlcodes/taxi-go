ffcd6c608dddb675f56fb8782a11e8f6cf003438
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-18 17:54:35 +0330

    add navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton showName afterSwitchSessionUrl="/" />
-      </SignedIn>
-    </div>
-  );
+  return <div></div>;
 }
